aa179f25d788cb671a83c56341d6636f8795df72
GustavKS <gstvschmidt@googlemail.com>
2025-03-21 17:24:09 +0100

    init2

---
+++ src/loss.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+
+class CLIPLoss(nn.Module):
+  def __init__(self):
+    super(CLIPLoss, self).__init__()
+    self.CE = nn.CrossEntropyLoss(reduction='mean')
+    self.temperature = 0.07
+
+  def forward(self, stim_emb, brain_emb):
+    targets = torch.arange(stim_emb.shape[0]).to(stim_emb.device)
+
+    ## To Dos: - Normalize embeddings
+
+    sim = torch.matmul(stim_emb, brain_emb.T)
+    sim /= self.temperature
+    loss = (self.CE(sim, targets) + self.CE(sim.T, targets)) / 2
+    return loss
--- src/train.py
+++ src/train.py
@@ -2,10 +2,13 @@ from omegaconf import OmegaConf
 import numpy as np
 
 import torch
+import wandb
+from tqdm import tqdm
 
-from models import Model
+#from models import Model
 from utils import parse_args, load_yaml_config, make_exp_folder
-from dataset import Dataset
+import dataset
+from loss import CLIPLoss
 
 
 if __name__ == "__main__":
@@ -16,6 +19,27 @@ if __name__ == "__main__":
 
   device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-  model = Model.Model().to(device)
+  dataset = dataset.BrennanDataset(config=config)
 
-  dataset = Dataset()
+  dataloader = torch.utils.data.DataLoader(dataset, batch_size=config['batch_size'], shuffle=True)
+
+  if config["use_wandb"]:
+    wandb.config = {k: v for k, v in config.items() if k not in ["use_wandb", "wandb_project"]}
+    wandb.init(project="BrainDecoding", config=wandb.config, save_code=True)
+    wandb.run.name = experiment_folder
+    wandb.run.save()
+
+  #brain_encoder = 
+  #stim_encoder =
+
+  loss_fn = CLIPLoss().to(device)
+
+  optimizer = torch.optim.Adam(brain_encoder.parameters(), lr=1e-4)
+
+  for epoch in range(100):
+    train_losses, test_losses = []
+
+    brain_encoder.train()
+
+    for i, batch in enumerate(tqdm(dataloader)):
+      stim, brain = batch
